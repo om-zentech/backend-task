bfac31155049d8c851c97e6153d2581bbd4c5294
MangukiyaOm22 <mangukiyaom22@gmail.com>
2026-01-28 18:02:52 +0530

    Initial Commit

---
+++ task-1.py
@@ -0,0 +1,48 @@
+# Stack Operations
+
+stack = []
+
+# Push Operation
+def push():
+    element = input("Enter Value: ")
+    stack.append(element)
+    print(f"{element} Push to stack")
+    print(f"Stack: {stack}")
+
+# Pop Operation
+def pop():
+    if len(stack) == 0:
+        print("Stack is Empty")
+    else:
+        element = stack.pop()
+        print(f"{element} Popped from stack")
+        print(f"Stack: {stack}")
+
+# Peep Operation
+def peep():
+    if len(stack) == 0:
+        print("Stack is Empty")
+    else:
+        print(f"Top Element: {stack[-1]}")
+
+# Choice Menu
+while True:
+    print("\nSelect Operation:")
+    print("1. Push")
+    print("2. Pop")
+    print("3. Peep")
+    print("4. Exit")
+    
+    choice = input("Enter choice: ")
+    
+    if choice == '1':
+        push()
+    elif choice == '2':
+        pop()
+    elif choice == '3':
+        peep()
+    elif choice == '4':
+        print("Exiting...")
+        break
+    else:
+        print("Invalid Choice! Try Again")
