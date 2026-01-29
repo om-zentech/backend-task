f6894aafcdc7f4c0fd893fb018b9d650096e0a51
om-zentech <om.mangukiya@zentechsolution.com>
2026-01-29 17:39:40 +0530

    [task 2]: Completed task 2 implementation of queue

---
+++ task-2.py
@@ -0,0 +1,51 @@
+'''
+Implement Queue You script should contain three functions
+1. enqueue
+2. dequeue
+3. peek 
+User will be given choice to perform action from above three. Script should be kept running until user chooses to exit.
+'''
+
+# Queue Operations
+
+queue = []
+
+# Enqueue Operation
+def enqueue():
+  element = input("Enter element: ")
+  queue.append(element)
+  print(f'{element} added to queue')
+  print(f'Queue: {queue}')
+
+# Dequeue Operation
+def dequeue():
+  if len(queue) == 0:
+    print("Queue is Empty")
+  else: 
+    print(f'{queue.pop(0)} removed from queue')
+    print(f'Queue: {queue}')
+
+# Peek (gives frist element of queue)
+def peek():
+  if len(queue) == 0:
+    print("Queue is Empty")
+  else:
+    print(f'{queue[0]} is first element of queue')
+
+# Choice Menu
+while True:
+
+  print("\n1.Enqueue / 2.Dequeue / 3.Peek / 4.Exit")
+  ch = input("Enter Choice: ")
+
+  if ch == '1':
+    enqueue()
+  elif ch == '2':
+    dequeue()
+  elif ch == '3':
+    peek()
+  elif ch == '4':
+    print("Exiting..")
+    break
+  else:
+    print("Enter valid choice, Try Agin")
---
+++ task-3.py
@@ -0,0 +1,70 @@
+'''
+Implement Circular Queue You script should contain four functions
+Front: Get the front item from queue.
+Rear: Get the last item from queue.
+enqueue(value)
+dequeue() 
+User will be given choice to perform action from above four. Script should be kept running until user chooses to exit.
+'''
+
+#Circular Queue Operations
+
+size = int(input("Enter size of queue: "))
+queue = [None]*size
+front = 0
+rear = 0
+n = 0
+
+# Enqueue Operation on Circular Queue
+def c_enqueue(value):
+  global rear,n
+  if front == rear and n == size:
+    print("Circular Queue Overflow!")
+  else:
+    queue[rear] = value
+    rear = (rear+1) % size
+    n += 1
+    print(f'{value} added to circular queue2')
+  print(f'Queue: {queue}')
+
+# Dequeue Operation on Circular Queue
+def c_dequeue():
+  global front,n
+  if front == rear and n == 0:
+    print("Circular queue Underflow!")
+  else:
+    value = queue[front]
+    queue[front] = None
+    front = (front+1) % size
+    n -= 1
+    print(f'{value} removed from circular queue')
+    print(f'Queue: {queue}')
+
+# Get Front (It returns front value from circular queue)
+def getFront():
+  print(f'Front is: {queue[front]}')
+
+# Get Raer (It returns raer value from circular queue)
+def getRear():
+  print(f'Rear is: {queue[rear]}')
+
+# Choice Menu
+while True:
+  
+  print("\n1.Enqueue / 2.Dequeue / 3.getFront / 4.getRear / 5.exit")
+  ch = input("Enter choice: ")
+
+  if ch == '1':
+    value = input("Enter value: ")
+    c_enqueue(value)
+  elif ch == '2':
+    c_dequeue()
+  elif ch == '3':
+    getFront()
+  elif ch == '4':
+    getRear()
+  elif ch == '5':
+    print("Exiting..")
+    break
+  else:
+    print("Invalid Choice, try again!")
