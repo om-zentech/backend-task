290ca6d0c611b92d36b0c94569aa61af73d8af68
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-04 19:41:13 +0530

    [task 10] Pelindrome number task completed

---
+++ task-10.py
@@ -0,0 +1,19 @@
+'''
+Pelindrome Number
+'''
+
+# Pelindrome Number
+
+n = int(input("Enter Number: "))
+temp = n  # Store number in temporary variable
+rev = 0
+
+while temp > 0: 
+  last_digit = temp % 10 # It gives last digit of number
+  rev = (rev*10) + last_digit # Reverse number
+  temp //= 10 # Remove last digit of number and store in temporary variable
+
+if n == rev:
+  print(f'{n} is Pelindrome Number')
+else:
+  print(f'{n} is Not a Pelindrome Number')
