7603b544789777561448881ea97e8911572a039c
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-16 10:34:08 +0530

    [task 17] Add time complexity

--- task-17.py
+++ task-17.py
@@ -13,9 +13,18 @@ def pattern(row):
   for i in range(1,row+1):  # this loop prints the pattern 
       print('*'*i)
   
-number_of_row = int(input('Enter number of row: ')) # take input of number of rows from user
-pattern(number_of_row)
-
+while True:
+  try:
+    number_of_row = int(input('Enter number of row: '))  # take input of number of rows from user
+    if number_of_row > 0:
+      pattern(number_of_row)
+    else:
+      print("Enter Positive number of rows only!")
+      continue
+    break
+  except ValueError:
+    print("Invalid input! Please enter a positive integer.")
+     
 '''
 time complexity: O(n^2)  
 because loop runs n times and in each iteration it prints i stars so 1+2+3+...+n = n(n+1)/2 = O(n^2)
