d82f8652b79bcf40832e17d5867f5bf9cc53ce7a
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-16 10:16:05 +0530

    [task 20] Add time complexity

--- task-20.py
+++ task-20.py
@@ -8,12 +8,18 @@ Task-20:
 '''
 
 # define a function for printing a pattern
-def pattern(row):
-  for i in range(row,0,-1): # this loop prints the pattern
-    print('*'*i)
-  
-number_of_row = int(input('Enter number of row: '))  # take input of number of rows from user
-if number_of_row > 0: 
-    pattern(number_of_row)
-else:
+def pattern():
+  number_of_row = int(input('Enter number of row: '))  # take input of number of rows from user
+  if number_of_row > 0: 
+    for i in range(number_of_row,0,-1): # this loop prints the pattern
+      print('*'*i)
+  else:
    print("Enter Positive number of rows only!")
+  
+  
+pattern()
+
+'''
+time complexity: O(n^2)
+because loop runs n times and in each iteration it prints i stars so 1+2+3+...+n = n(n+1)/2 = O(n^2)
+'''
