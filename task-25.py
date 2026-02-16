7517689ea1fb9a468a408d48527219601bcffd04
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-16 10:36:43 +0530

    [task 25] Pascal pyramid pattern task completed

--- task-25.py
+++ task-25.py
@@ -17,12 +17,19 @@ def pattern(row):
       print(number,end=' ')
       number = number * (i - k) // (k + 1)
     print()
-number_of_row = int(input('Enter number of row: '))  # take input of number of rows from user
-if number_of_row > 0:
-    pattern(number_of_row)
-else:
-    print("Enter Positive number of rows only!")
-
+    
+while True:
+  try:
+    number_of_row = int(input('Enter number of row: '))  # take input of number of rows from user
+    if number_of_row > 0:
+      pattern(number_of_row)
+    else:
+      print("Enter Positive number of rows only!")
+      continue
+    break
+  except ValueError:
+    print("Invalid input! Please enter a positive integer.")
+    
 '''
 time complexity: O(n^2)
 because loop runs n times and in each iteration it prints i numbers so 1+2+3+...+n = n(n+1)/2 = O(n^2)
