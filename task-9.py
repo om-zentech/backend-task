42ce59763424905aa786606beac6753483ac6aac
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-17 10:52:54 +0530

    [fix task 9] enhancement: wrap everything in function & add validation on input

--- task-9.py
+++ task-9.py
@@ -4,20 +4,32 @@ Find Diterminent of 3*3 Matrix.
 
 # Determinent of 3*3 Matrix
 
-m = []
 
-# Take user input to fill the matrix
-print('\nMatrix-1')
-for i in range(3):
-  m1 = []
-  for j in range(3):
-    a = int(input(f'Enter Value of [{i+1,j+1}]: '))
-    m1.append(a)
-  m.append(m1) # Add every row to list
+def determinant():
+  matrix = []
 
-ans = (m[0][0]*(m[1][1] * m[2][2] - m[1][2] * m[2][1]) 
-      - m[0][1]*(m[1][0] * m[2][2] - m[1][2] * m[2][0]) 
-      + m[0][2]*(m[1][0] * m[2][1] - m[1][1] * m[2][0]))  # Formula for finding Determinent
+  # Take user input to fill the matrix
+  print('\nMatrix-1')
+  for i in range(3):
+    matrix_array = []
+    for j in range(3):
+      matrix_value = (input(f'Enter Value of [{i+1,j+1}]: '))
 
-print(f'Matrix: {m}')
-print(f'Determinent: {ans}')
+      while True:
+        if matrix_value.isdigit() == False:
+          print('Invalid input, Please enter only numbers')
+          matrix_value = (input(f'Enter Value of [{i+1,j+1}]: '))
+        else:
+          break
+        
+      matrix_array.append(int(matrix_value))
+    matrix.append(matrix_array) # Add every row to list
+
+  ans = (matrix[0][0]*(matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) 
+        - matrix[0][1]*(matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) 
+        + matrix[0][2]*(matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]))  # Formula for finding Determinent
+
+  print(f'Matrix: {matrix}')
+  return f'Determinent: {ans}'
+
+print(determinant())
