94d29165bbf86092f1cbbda5fafb63528e193c97
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-17 10:50:07 +0530

    [fix task 8] enhancement: wrap everything in function & give variable meaningful name

--- task-8.py
+++ task-8.py
@@ -9,43 +9,51 @@ Matrix Multiplication
 
 # 3*3 Matrix Multiplication with User Input
 
-matrix1 = []
-matrix2 = []
-result = [[0,0,0],[0,0,0],[0,0,0]]
-
-# Take user input to fill the matrix
-def matrix_input():
+# define a function for matrix multiplication
+def matrix_multiplication():
+  
+  matrix1 = []
+  matrix2 = []
+  result = [[0,0,0],[0,0,0],[0,0,0]]
+  
   print('\nMatrix-1') # Matrix-1
   for i in range(3):
-    m1 = []
+    matrix1_array = []
     for j in range(3):
-      a = int(input(f'Enter Value of [{i+1,j+1}]: '))     
-      m1.append(a)
-    matrix1.append(m1) # Add every row to list 
+      matrix1_value = (input(f'Enter Value of [{i+1,j+1}]: ')) 
+      while True:
+        if matrix1_value.isdigit() == False:
+          print('Invalid input, Please enter only numbers')
+          matrix1_value = (input(f'Enter Value of [{i+1,j+1}]: '))
+        else:    
+          matrix1_array.append(int(matrix1_value))
+          break
+    matrix1.append(matrix1_array) # Add every row to list 
 
   print('\nMatrix-2') # Matrix-2
   for x in range(3):
-    m2 = []
+    matrix2_array = []
     for y in range(3):
-      b = int(input(f'Enter Value of [{x+1,y+1}]: '))     
-      m2.append(b)
-    matrix2.append(m2) # Add every row to list
+      matrix2_value = (input(f'Enter Value of [{x+1,y+1}]: '))     
+      while True:
+        if matrix2_value.isdigit() == False:
+          print('Invalid input, Please enter only numbers')
+          matrix2_value = (input(f'Enter Value of [{x+1,y+1}]: '))
+        else:
+          matrix2_array.append(int(matrix2_value))
+          break
+    matrix2.append(matrix2_array) # Add every row to list
 
 # Multiply both matrix
-def matrix_multiplication():
   for i in range(3):
     for j in range(3):
       for k in range(3):
         result[i][j] += matrix1[i][k] * matrix2[k][j] # Multiply matrix-1 with matrix-2
 
 # Print the multiplied matrix
-def matrix_result():
   print()
   print(f'Matrix-1: {matrix1}')
   print(f'Matrix-2: {matrix2}')
   print(f'\nMatrix-1 * Matrix-2: {result}')
 
-
-matrix_input()
 matrix_multiplication()
-matrix_result()
