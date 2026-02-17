17b78784d6bd0df0d0ccffff1e2bb8230de469a7
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-17 10:43:47 +0530

    [fix task 4] enhancement: give variables meaningful names & optimize code

--- task-4.py
+++ task-4.py
@@ -7,16 +7,21 @@ Use selection sort to sort this list
 
 # Selection Sort
 
-l = [2,5,3,1,6,9] # Static List
-n = len(l)
+def selection_sort():
+  static_list = [2,5,3,1,6,9] # Static List
 
-for i in range(n):
-  min = i
-  print(f'Step-{i+1}:\n{l}')
-  for j in range(i+1,n): # Find Minimum Element from List
-    if l[min] > l[j]: 
-      min = j 
-  print(f'Minimum Element: {l[min]}\n')
-  l[min],l[i] = l[i],l[min] # Swaping with Minmum Element
-  
-print(f'Sorted List Using Selection Sort: {l}')
+  for i in range(len(static_list)-1):
+
+    min_value = i
+    print(f'Step-{i+1}:\n{static_list}')
+
+    for j in range(i+1,len(static_list)): # Find Minimum Element from List
+      if static_list[min_value] > static_list[j]: 
+        min_value = j 
+
+    print(f'Minimum Element: {static_list[min_value]}\n')
+    static_list[min_value],static_list[i] = static_list[i],static_list[min_value] # Swaping with Minmum Element
+    
+  return f'Sorted List Using Selection Sort: {static_list}'
+
+print(selection_sort())
