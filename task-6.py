7db5c50b3294df27ad010cdf796434938c454d00
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-17 10:46:18 +0530

    [fix task 6] enhancement: fix the order of conditions & give variable meaningful names

--- task-6.py
+++ task-6.py
@@ -6,14 +6,18 @@ Use insertion sort to sort this list
 '''
 # Insertion Sort
 
-l = [5,12,7,3,2,15]
-n = len(l)
+def insertion_sort():
+  static_list = [5,12,7,3,2,15]
 
-for i in range(1,n):
-  temp = l[i]
-  j = i-1
-  while temp < l[j] and j >= 0: # compare every element before that element
-    l[j+1] = l[j] # shift element 
-    j -= 1
-  l[j+1] = temp # place element to it's right place
-print(f'Sorted List using Insertion sort: {l}')
+  for i in range(1,len(static_list)):
+    copy_list = static_list[i]
+    j = i-1
+
+    while j >= 0 and copy_list < static_list[j]: # compare every element before that element
+      static_list[j+1] = static_list[j] # shift element 
+      j -= 1
+
+    static_list[j+1] = copy_list # place element to it's right place
+  return f'Sorted List using Insertion sort: {static_list}'
+
+print(insertion_sort())
