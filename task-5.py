964544f544377927c53ab83d5c0a29093585d37c
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-17 10:44:14 +0530

    [fix task 5] enhancement: give variables meaningful names & optimize code

--- task-5.py
+++ task-5.py
@@ -7,15 +7,19 @@ Use bubble sort to sort this list
 
 # Bubble Sort
 
-l = [2,5,3,10,1,6,9] # Static List
-n = len(l)
+def bubble_sort():
+  static_list = [2,5,3,10,1,6,9] # Static List
 
-for i in range(n):
-  if i == n-1:
-    print(f'\nSorted List Using Bubble Sort: {l}')
-  else: 
-    print(f'\nStep-{i+1}:') # Prints Every Steps of swapping
-  for j in range(0,n-i-1):
-    if l[j] > l[j+1]:
-      l[j],l[j+1] = l[j+1],l[j] # Swapping Element
-    print(f'{l}')
+  for i in range(len(static_list)-1):
+
+    if i == len(static_list)-1:
+      print(f'\nSorted List Using Bubble Sort: {static_list}')
+    else: 
+      print(f'\nStep-{i+1}:') # Prints Every Steps of swapping
+
+    for j in range(0,len(static_list)-i-1):
+      if static_list[j] > static_list[j+1]:
+        static_list[j],static_list[j+1] = static_list[j+1],static_list[j] # Swapping Element
+      print(f'{static_list}')
+
+print(bubble_sort())
