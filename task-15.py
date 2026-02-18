7fb6ec5cffd0afc6bb29c2c397e20b29c1d5f035
om-zentech <om.mangukiya@zentechsolution.com>
2026-02-18 16:03:14 +0530

    [fix task 15] add logic for finding factors of negative numbers

--- task-15.py
+++ task-15.py
@@ -8,11 +8,20 @@ import math
 # define a function for finding factors of a number
 def factors_of_number(number):
   list_of_factors = []
-  for i in range(1,int(math.sqrt(number)+1)): # 
-    if number % i == 0:
-      list_of_factors.append(i) # add factor to list if number is divisible by i
-      list_of_factors.append(number // i)
-  return list_of_factors
+  if number < 0:
+    for i in range(1,int(math.sqrt(-number)+1)):
+      if -number % i == 0:
+        list_of_factors.append(i) # add factor to list if number is divisible by i
+        list_of_factors.append(-i) # add negative factor to list
+        list_of_factors.append((-number) // i) # add factor to list if number is divisible by i
+        list_of_factors.append(-((-number) // i)) # add negative factor to list
+    return list_of_factors
+  else:
+    for i in range(1,int(math.sqrt(number)+1)): # 
+      if number % i == 0:
+        list_of_factors.append(i) # add factor to list if number is divisible by i
+        list_of_factors.append(number // i)
+    return list_of_factors
 
 # take input of a number from user
 while True:
